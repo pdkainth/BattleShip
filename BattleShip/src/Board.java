fd79d6dceed4044a1941042fd4c6fbec38643b86
Prabhdeep Kainth <pdkainth@gmail.com>
2019-05-08 18:40:39 -0700

    Created board class and tester class

--- BattleShip/src/Board.java
+++ BattleShip/src/Board.java
@@ -1,8 +1,54 @@
 
 public class Board {
 
-	private String[][] board = new String[12][12];
+	private final int SIDE = 10;
+	private String[][] board = new String[SIDE][SIDE];
+	private final String EMPTY = "-";
+	private final String HIT = "X";
+	private final String MISS = "O";
 	
+	
+	
+	
+	public Board() {
+		for(int row = 0; row < board.length; row++) {
+			for(int col = 0; col < board[row].length; col++) {
+				board[row][col] = EMPTY;
+			}
+		}
+	}
+	
+	public void printBoard() {
+		System.out.print("  ");
+		for(char row = 'a'; row < 'k'; row++) {
+			System.out.print(row + " ");
+		}
+		System.out.println();
+		
+		for(int row = 0; row < board.length; row++) {
+			System.out.print(row + " ");
+			for(int col = 0; col < board[row].length; col++) {
+				System.out.print(board[row][col] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public boolean alreadyChosen(int row, int col) {
+		if(board[row][col] == "-") {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public void hit(int row, int col) {
+		board[row][col] = HIT;
+	}
+	
+	public void miss(int row, int col) {
+		board[row][col] = MISS;
+	}
 	
 	
 }
---
+++ BattleShip/src/Tester.java
@@ -0,0 +1,11 @@
+
+public class Tester {
+
+	public static void main(String args[]) {
+		Board b = new Board();
+		b.hit(0, 8);
+		b.miss(3, 4);
+		b.printBoard();
+
+	}
+}
