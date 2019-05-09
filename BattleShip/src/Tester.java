e78b982ab073a21e3fd364e710a6e7dd27b9a88a
NubCoder <36945035+NubCoder@users.noreply.github.com>
2019-05-09 11:24:14 -0700

    Update Tester.java

--- BattleShip/src/Tester.java
+++ BattleShip/src/Tester.java
@@ -1,11 +1,46 @@
+import java.util.*;
 
 public class Tester {
 
 	public static void main(String args[]) {
 		Board b = new Board();
-		b.hit(0, 8);
-		b.miss(3, 4);
+
 		b.printBoard();
+		
+		System.out.println("Placing ships now");
+		
+		b.placeShips();
+		
+		b.printBoard();
+		
+		Scanner in = new Scanner(System.in);
+		
+		
+		
+		
+		while (!b.winnerExists()) {
+			System.out.print("Where would you like to hit?");
+
+			int xpos = in.nextInt();
+			int ypos = in.nextInt();
+
+			
+			if (!b.alreadyChosen(xpos, ypos)) {
+				System.out.println("Ok");
+				
+				b.checkPosition(xpos, ypos);
+				
+			}else {
+				System.out.println("Enter another location. That location is already taken or is invalid.");
+			}
+			
+			b.printBoard();
+			
+		}
+		
+		System.out.println("WE HAVE A WINNER");
+		System.out.println("CONGRATS!!!");
+		in.close();
 
 	}
 }
