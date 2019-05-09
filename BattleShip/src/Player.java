9e6f1e22d43308ef88cbc4bc7bf0dbdafed23e91
Abhig2002 <34994192+Abhig2002@users.noreply.github.com>
2019-05-09 11:27:43 -0700

    Create Player.java

---
+++ BattleShip/src/Player.java
@@ -0,0 +1,39 @@
+mport java.util.*;
+
+public class Player extends Ships{
+	
+	private String name;
+	private int turn;
+	private int numberShipsLeft;
+	Random r = new Random();
+	
+	
+	public Player(String name) {
+		super();
+		this.name = name;
+
+	}	
+	
+	public void setCarrier() {
+		
+	}
+	
+	public void setBattle() {
+		
+	}
+
+	public void setCruiser() {
+		
+	}
+
+	public void setSub() {
+		
+	}
+
+	public void setDestroyer() {
+		
+	}
+	
+	
+}
+
