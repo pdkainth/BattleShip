cad0587681e97d0a4f08bb5c5a5a684fb2dffe98
Abhig2002 <34994192+Abhig2002@users.noreply.github.com>
2019-05-10 10:55:13 -0700

    Update Board.java

--- BattleShip/src/Board.java
+++ BattleShip/src/Board.java
@@ -11,6 +11,9 @@ public class Board {
 
 	Random r = new Random();
 	
+	/**
+	 *This method creates an instance of a Board object which sets up th game board for botht he player and the CPU.
+	 */
 	public Board() {
 		for(int row = 0; row < board.length; row++) {
 			for(int col = 0; col < board[row].length; col++) {
@@ -18,7 +21,9 @@ public class Board {
 			}
 		}
 	}
-	
+	/**
+	 * Prints the board layout for both the player and CPU, with current layout
+	 */
 	public void printBoard() {
 		System.out.print("  ");
 		for(int row = 0; row < SIDE; row++) {
@@ -34,7 +39,11 @@ public class Board {
 			System.out.println();
 		}
 	}
-	
+	/**
+	 * @param row - row number of the board
+	 * @param col - column number of the board
+	 * @return boolean value for determining a taken space or an empty space
+	 */
 	public boolean alreadyChosen(int row, int col) {
 		try {
 			if(board[row][col] == EMPTY || board[row][col] == "S") {
@@ -48,14 +57,27 @@ public class Board {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param row - row number of the board
+	 * @param col - column number of the board
+	 */
 	public void hit(int row, int col) {
 		board[row][col] = HIT;
 	}
 	
+	/**
+	 * 
+	 * @param row - number of rows on the board
+	 * @param col - number of columns on the board
+	 */
 	public void miss(int row, int col) {
 		board[row][col] = MISS;
 	}
 	
+	/**
+	 * This method allows the user to place ships on the game board with user input
+	 */
 	public void placeShips() {
 		
 		for (int i = 0; i < 4; i++) {
@@ -66,6 +88,11 @@ public class Board {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param x - the position number for the column
+	 * @param y- the position number for the row
+	 */
 	public void checkPosition(int x, int y) {
 		if (board[x][y] == EMPTY) {
 			miss(x, y);
@@ -75,6 +102,10 @@ public class Board {
 		}
 	}
 	
+	/**
+	 * This method determines who the winner is by looping through every element of the board, checking for any ships remaining
+	 * @return - boolean value determining the existence of any remaining ships
+	 */
 	public boolean winnerExists() {
 		for (int i = 0; i < SIDE; i++) {
 			for (int k = 0; k < SIDE; k++) {
@@ -87,6 +118,10 @@ public class Board {
 		return true;
 	}
 	
+	/**
+	 * 
+	 * @return - an array of Strings determining spots taken by ships, spots that are empty, and spots that have been hit or miss
+	 */
 	public String[][] getBoard(){
 		return board;
 	}
@@ -95,3 +130,4 @@ public class Board {
 	
 	
 }
+
